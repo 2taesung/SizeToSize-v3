a7758e6fd579cfb2850f65a9e7aaf08c0e6bb862
owaldeff <tialem94@gmail.com>
2021-06-29 23:34:02 +0900

    update logins.py in server folder

--- server/django_backend/social_login/logins.py
+++ server/django_backend/social_login/logins.py
@@ -8,50 +8,37 @@ import requests
 
 # Create your views here.
 
-class SocialLogin:
-    platforms = ['kakao']
+
+@csrf_exempt
+@require_http_methods(['GET'])
+def kakao_login(self):
+    # access_token
+    try:
+        access_token = request.headers['Token']
+    except KeyError:
+        return JsonResponse({'message': 'HEADERS KAKAO TOKEN KEY ERROR'}, status=400)
+    # profile
+    profile_url = 'https://kapi.kakao.com/v2/user/me'
+    headers = {'Authorization' : f'Bearer {access_token}'}
+    profile_request = requests.get(profile_url, headers=headers)
+    profile_json = profile_request.json()
+    try:
+        kakao_id = profile_json['id']
+        kakao_nickname = profile_json['properties']['nickname']
+    except KeyError:
+        return JsonResponse({'message': 'KAKAO API PROFILE KEY ERROR'}, status=400)
+    # User, access_jwt
+    User_search = User.objects.filter(kakao_id=kakao_id)
+    access_jwt = jwt_publish(kakao_id, access_token)
+    if User_search:
+        pass
+    else:
+        User.objects.create(kakao_id=kakao_id, kakao_nickname=kakao_nickname)
     
-    @csrf_exempt
-    @require_http_methods(['GET'])
-    def __init__(self):
-        if request.headers['Platform'].lower() not in self.platforms:
-            return JsonResponse({'message': 'LOGIN KEY ERROR'}, status=400)
-        
-        if request.headers['Platform'] == 'kakao':
-            return self.kakao_login
-        
-        # if request.headers['Login'] == 'naver':
-        #     return self.naver_login
-    
-    @csrf_exempt
-    def kakao_login(self):
-        # access_token
-        try:
-            access_token = request.headers['Token']
-        except KeyError:
-            return JsonResponse({'message': 'HEADERS KAKAO TOKEN KEY ERROR'}, status=400)
-        # profile
-        profile_url = 'https://kapi.kakao.com/v2/user/me'
-        headers = {'Authorization' : f'Bearer {access_token}'}
-        profile_request = requests.get(profile_url, headers=headers)
-        profile_json = profile_request.json()
-        try:
-            kakao_id = profile_json['id']
-            kakao_nickname = profile_json['properties']['nickname']
-        except KeyError:
-            return JsonResponse({'message': 'KAKAO API PROFILE KEY ERROR'}, status=400)
-        # User, access_jwt
-        User_search = User.objects.filter(kakao_id=kakao_id)
-        access_jwt = jwt_publish(kakao_id, access_token)
-        if User_search:
-            pass
-        else:
-            User.objects.create(kakao_id=kakao_id, kakao_nickname=kakao_nickname)
-        
-        # response = JsonResponse({'message': 'KAKAO LOGIN SUCESS'}, status=201)
-        # response.set_cookie('access_token', value=access_token, max_age=60*60*24*7, expires='None', path='/', domain='None', httponly=False, samesite='None')
-        # response.set_cookie('access_jwt', value=access_jwt, max_age=60*60*24*7, expires='None', path='/', domain='None', httponly=False, samesite='None')
-        return JsonResponse({'message': 'KAKAO LOGIN SUCESS', 'Authorization': access_jwt}, status=201)
-    
-    # def naver_login(self):
-    #     pass
+    # response = JsonResponse({'message': 'KAKAO LOGIN SUCESS'}, status=201)
+    # response.set_cookie('access_token', value=access_token, max_age=60*60*24*7, expires='None', path='/', domain='None', httponly=False, samesite='None')
+    # response.set_cookie('access_jwt', value=access_jwt, max_age=60*60*24*7, expires='None', path='/', domain='None', httponly=False, samesite='None')
+    return JsonResponse({'message': 'KAKAO LOGIN SUCESS', 'Authorization': access_jwt}, status=201)
+
+# def naver_login(self):
+#     pass
